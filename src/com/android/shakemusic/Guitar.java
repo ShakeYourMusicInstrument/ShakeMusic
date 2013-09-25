177c1da4caf4cea4050358d94b0d072c05bf6023
adrian1992 <adrian1992@gmail.com>
2013-09-25 20:27:41 +0200

    guitar fixes

--- src/com/android/shakemusic/Guitar.java
+++ src/com/android/shakemusic/Guitar.java
@@ -1,25 +1,23 @@
 package com.android.shakemusic;
 
-public class Guitar {
+public class Guitar implements Instrument{
 
 	private int nHarm;
 	private int bpm;
-	private final int fs = 44100;
-	private final double inharmonity = 0.01;
-	private final double gam = 1.7;
-	private final double pi = Math.PI;
-	private final double pisqr = pi * pi;
-
-	Guitar(int nHarm, int bpm) {
+	private double duration;
+	private double pluck_location;
+	
+	Guitar(int nHarm, int bpm, double duration, double pluck_location) {
 		this.nHarm = nHarm;
 		this.bpm = bpm;
+		this.duration = duration;
+		this.pluck_location = pluck_location;
 	}
 
-	public short[] Note(int freq, double pluck_location) {
-
+	public short[] Note(int freq) {
 		int Nt, jh, jhsqr;
 		double T, An, dfn;
-		T = 60 / bpm;
+		T = duration / bpm;
 		Nt = (int) (fs * T);
 		short sj[] = new short[Nt];
 		// Make Note
@@ -29,14 +27,13 @@ public class Guitar {
 			for (j = 0; j < nHarm; j++) {
 				jh = j + 1;
 				jhsqr = jh * jh;
-				An = 2
-						/ ((pisqr) * jhsqr * pluck_location * (1 - pluck_location))
+				An = 2	/ ((pisqr) * jhsqr * pluck_location * (1 - pluck_location))
 						* Math.sin(jh * pi * pluck_location);
 				dfn = Math.sqrt(1 + jhsqr * inharmonity * inharmonity);
 				sj[i] = (short) (An * Math.exp(-gam * jh * i / fs) * Math.sin(2
 						* pi * i * freq * dfn * jh / fs));
 			}
 		}
-		return null;
+		return sj;
 	}
 }
