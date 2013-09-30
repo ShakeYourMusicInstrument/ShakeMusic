a86806dea588cb6ee7b50cacaac9ee6a2d58cba8
Romina <rominamuka@gmail.com>
2013-09-30 22:24:12 +0200

    Menu Changed

--- src/com/android/shakemusic/MainActivity.java
+++ src/com/android/shakemusic/MainActivity.java
@@ -5,9 +5,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 
@@ -20,42 +19,37 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		MenuInflater inflater=getMenuInflater();
+	    inflater.inflate(R.menu.main, menu);	
+	    return true;
 		
-		
+	}
 		/*
 		 * onClick Guitar
 		 */
-		ImageButton guitar = (ImageButton)findViewById(R.id.guitar);
-	  	guitar.setOnClickListener(new OnClickListener() {
-	  	public void onClick(View v) {
+		
+	  	public void onClickGuitar(View v) {
 	  			Intent guitar = new Intent(MainActivity.this, GuitarActivity.class);
 	  			startActivity(guitar);
-	  	}});
+	  	}
 	  	
 	  	/*
 		 * onClick Play
 		 */
-		ImageButton play = (ImageButton)findViewById(R.id.play);
-	  	play.setOnClickListener(new OnClickListener() {
-	  	public void onClick(View v) {
+		
+	  	public void onClickPlay(View v) {
 	  			Intent play = new Intent(MainActivity.this, PlayActivity.class);
 	  			startActivity(play);
-	  	}});
+	  	}
 	  	
 	  	/*
 		 * onClick Piano
 		 */
-		ImageButton piano = (ImageButton)findViewById(R.id.piano);
-	  	piano.setOnClickListener(new OnClickListener() {
-	  	public void onClick(View v) {
+		
+	  	public void onClickPiano(View v) {
 	  			Intent piano = new Intent(MainActivity.this, PianoActivity.class);
 	  			startActivity(piano);
-	  	}});
+	  	}
 	  	
-	  	
-	  	return true;
-		
-	}
 
 }
