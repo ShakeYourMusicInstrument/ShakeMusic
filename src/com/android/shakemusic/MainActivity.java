eab951daed747ae5eff37c2694febcc98992d4ab
Romina <rominamuka@gmail.com>
2013-10-01 19:17:11 +0200

    Menu Items

--- src/com/android/shakemusic/MainActivity.java
+++ src/com/android/shakemusic/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends Activity {
@@ -19,16 +20,33 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
+		super.onCreateOptionsMenu(menu);
 		MenuInflater inflater=getMenuInflater();
 	    inflater.inflate(R.menu.main, menu);	
 	    return true;
 		
 	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch(item.getItemId()){
+		case R.id.language:
+			
+			break;
+		case R.id.instructions:
+			Intent i = new Intent("com.android.shakemusic.INSTRUCTION");
+			startActivity(i);
+			break;
+		}
+		
+		return false;
+	}
+	
 		/*
 		 * onClick Guitar
 		 */
-		
-	  	public void onClickGuitar(View v) {
+
+	public void onClickGuitar(View v) {
 	  			Intent guitar = new Intent(MainActivity.this, GuitarActivity.class);
 	  			startActivity(guitar);
 	  	}
