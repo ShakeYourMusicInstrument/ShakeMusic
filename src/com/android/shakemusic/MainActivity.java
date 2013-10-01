d01b360e3a46ec8909be2deeecbd6af111880d17
adrian1992 <adrian1992@gmail.com>
2013-10-01 19:54:06 +0200

    cleaner code

--- src/com/android/shakemusic/MainActivity.java
+++ src/com/android/shakemusic/MainActivity.java
@@ -19,37 +19,37 @@ public class MainActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		MenuInflater inflater=getMenuInflater();
-	    inflater.inflate(R.menu.main, menu);	
-	    return true;
-		
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return true;
+
 	}
-		/*
-		 * onClick Guitar
-		 */
-		
-	  	public void onClickGuitar(View v) {
-	  			Intent guitar = new Intent(MainActivity.this, GuitarActivity.class);
-	  			startActivity(guitar);
-	  	}
-	  	
-	  	/*
-		 * onClick Play
-		 */
-		
-	  	public void onClickPlay(View v) {
-	  			Intent play = new Intent(MainActivity.this, PlayActivity.class);
-	  			startActivity(play);
-	  	}
-	  	
-	  	/*
-		 * onClick Piano
-		 */
-		
-	  	public void onClickPiano(View v) {
-	  			Intent piano = new Intent(MainActivity.this, PianoActivity.class);
-	  			startActivity(piano);
-	  	}
-	  	
+
+	/*
+	 * onClick Guitar
+	 */
+
+	public void onClickGuitar(View v) {
+		Intent guitar = new Intent(MainActivity.this, GuitarActivity.class);
+		startActivity(guitar);
+	}
+
+	/*
+	 * onClick Play
+	 */
+
+	public void onClickPlay(View v) {
+		Intent play = new Intent(MainActivity.this, PlayActivity.class);
+		startActivity(play);
+	}
+
+	/*
+	 * onClick Piano
+	 */
+
+	public void onClickPiano(View v) {
+		Intent piano = new Intent(MainActivity.this, PianoActivity.class);
+		startActivity(piano);
+	}
 
 }
