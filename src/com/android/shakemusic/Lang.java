c34d2301dd266c05a5aae3127ee1a5a07a78c411
Romina <rominamuka@gmail.com>
2013-10-04 17:41:47 +0200

    manual language

--- src/com/android/shakemusic/Lang.java
+++ src/com/android/shakemusic/Lang.java
@@ -1,7 +1,12 @@
 package com.android.shakemusic;
 
+import java.util.Locale;
+
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Lang extends PreferenceActivity {
 
@@ -12,5 +17,42 @@ public class Lang extends PreferenceActivity {
 		addPreferencesFromResource(R.xml.lang);
 		
 	}
+	
+	 @Override
+	    public boolean onOptionsItemSelected(MenuItem item) {
+	        switch(item.getItemId()){
+
+	        case 1:
+
+	            Locale locale = new Locale("us"); 
+	            Locale.setDefault(locale);
+	            Configuration config = new Configuration();
+	            config.locale = locale;
+	            getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+	            Toast.makeText(this, "Local in English!", Toast.LENGTH_LONG).show();
+	            break;
+
+	        case 2:
+
+	            Locale locale2 = new Locale("al"); 
+	            Locale.setDefault(locale2);
+	            Configuration config2 = new Configuration();
+	            config2.locale = locale2;
+	            getBaseContext().getResources().updateConfiguration(config2, getBaseContext().getResources().getDisplayMetrics());
+	            Toast.makeText(this, "Lokale në Shqip!", Toast.LENGTH_LONG).show();
+	            break;  
+
+	        case 3:
+
+	            Locale locale3 = new Locale("es"); 
+	            Locale.setDefault(locale3);
+	            Configuration config3 = new Configuration();
+	            config3.locale = locale3;
+	            getBaseContext().getResources().updateConfiguration(config3, getBaseContext().getResources().getDisplayMetrics());
+	            Toast.makeText(this, "Local en Espagna!", Toast.LENGTH_LONG).show();
+	            break;  
+	        }
+	        return super.onOptionsItemSelected(item);
+	    }
 
 }
