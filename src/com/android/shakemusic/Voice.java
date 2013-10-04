74cfb5dca5096397cadd189cf4d09dcd11207bca
dianagtf <diana.gtf88@gmail.com>
2013-10-04 16:01:19 +0200

    Changes language voice

--- src/com/android/shakemusic/Voice.java
+++ src/com/android/shakemusic/Voice.java
@@ -17,7 +17,7 @@ public class Voice {
 			public void onInit(int status) {
 				// TODO Auto-generated method stub
 				if (status != TextToSpeech.ERROR) {
-					tts.setLanguage(Locale.US);
+					tts.setLanguage(tts.getLanguage());
 				}
 				tts.speak(ctx2.getString(R.string.InstructionListen),
 						TextToSpeech.QUEUE_FLUSH, null);
